54fea99f06482e9fadc96d7465109c52c6c4cd00
Paul3409 <121635306+Paul3409@users.noreply.github.com>
2026-03-20 13:01:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -368,7 +368,7 @@ class ColormapRegistry:
 
     @staticmethod
     def get_snow_depth() -> mcolors.LinearSegmentedColormap:
-        colors = ['#FFFFFF00', '#E0FFFF', '#AFEEEE', '#00FFFF', '#1E90FF', '#0000FF', '#8A2BE2', '#FF00FF']
+        colors = ['#FFFFFF00', '#fff0f5', '#dda0dd', '#00FFFF', '#1E90FF', '#0000FF', '#8A2BE2', '#FF00FF']
         cmap = mcolors.LinearSegmentedColormap.from_list("snow_scale", colors)
         cmap.set_bad(color='none')
         return cmap
